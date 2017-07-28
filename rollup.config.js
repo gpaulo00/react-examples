d42da3449adf38a6cfd9a58d6f122fb3b37867bb
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2017-07-28 01:48:55 -0400

    configure rollup for npm modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 
+import resolve from "rollup-plugin-node-resolve"
+import commonjs from "rollup-plugin-commonjs"
 import babel from "rollup-plugin-babel"
 
 export default {
-  entry: "src/index.js",
-  format: "iife",
-  dest: "build/bundle.js",
-  external: ["react", "react-dom"],
-  plugins: [
-    babel({
-      exclude: "node_modules/**"
-    })
+  entry : "src/index.js",
+  format : "iife",
+  dest : "build/bundle.js",
+  plugins : [
+    resolve(),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({exclude: "node_modules/**"})
   ]
 }
