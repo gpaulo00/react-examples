e8397945f99a5d24f6278135f5856e7f3bfa4a7f
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2017-07-28 02:57:35 -0400

    fixing package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-
 import resolve from "rollup-plugin-node-resolve"
 import commonjs from "rollup-plugin-commonjs"
 import replace from "rollup-plugin-replace"
 import babel from "rollup-plugin-babel"
 import uglify from "rollup-plugin-uglify"
 
+
 export default {
   entry : "src/index.js",
   format : "iife",
   dest : "build/bundle.js",
-  sourceMap: true,
+  sourceMap : true,
   plugins : [
     resolve({
       jsnext: true
@@ -17,7 +17,9 @@ export default {
     commonjs({
       include: 'node_modules/**'
     }),
-    babel({exclude: "node_modules/**"}),
+    babel({
+      exclude: "node_modules/**"
+    }),
     replace({
       'process.env.NODE_ENV': JSON.stringify("production")
     }),
