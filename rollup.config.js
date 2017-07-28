da72c2b5f037c7cd5b52c7e1bb106504cfa73fbe
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2017-07-28 02:05:53 -0400

    bundle working
    add replace plugin for rollup
    add source maps

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,24 @@
 
 import resolve from "rollup-plugin-node-resolve"
 import commonjs from "rollup-plugin-commonjs"
+import replace from "rollup-plugin-replace"
 import babel from "rollup-plugin-babel"
 
 export default {
   entry : "src/index.js",
   format : "iife",
   dest : "build/bundle.js",
+  sourceMap: true,
   plugins : [
     resolve({
-      jsnext: true, main: true
+      jsnext: true
     }),
     commonjs({
       include: 'node_modules/**'
     }),
-    babel({exclude: "node_modules/**"})
+    babel({exclude: "node_modules/**"}),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify("production")
+    })
   ]
 }
